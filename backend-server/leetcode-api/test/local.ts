4b444130233275de0b0962016f1503e06166754c
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-31 18:44:29 -0400

    refactore project structure

--- backend-server/leetcode-api/test/local.ts
+++
@@ -1,8 +0,0 @@
-// // local.ts
-// import app from "./app";
-
-// const PORT = process.env.PORT || 3002;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
