c4f0ce2aad56eca72d921505f444abca8f069288
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-31 18:23:05 -0400

    Fix(lambda.handler) vendia serverless-express export handler

--- backend-server/leetcode-api/aws-lambda-service/dist/lambda.js
+++ backend-server/leetcode-api/aws-lambda-service/dist/lambda.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const serverless_express_1 = __importDefault(require("@vendia/serverless-express"));
-const app_1 = __importDefault(require("./app")); // your Express app
+const app_1 = __importDefault(require("./app"));
 exports.handler = (0, serverless_express_1.default)({ app: app_1.default });
--- backend-server/leetcode-api/aws-lambda-service/lambda.ts
+++ backend-server/leetcode-api/aws-lambda-service/lambda.ts
@@ -1,7 +1,3 @@
 import serverlessExpress from "@vendia/serverless-express";
 import app from "./app";
-import type { Handler, APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
-
-const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = serverlessExpress({ app });
-
-export { handler };
+export const handler = serverlessExpress({ app });
