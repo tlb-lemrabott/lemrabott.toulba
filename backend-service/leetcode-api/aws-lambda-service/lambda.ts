4ade01f20d2b7c93399b24f69bc4fdf832c72814
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-31 19:40:49 -0400

    Test - update lambd handler

--- backend-service/leetcode-api/aws-lambda-service/lambda.ts
+++ backend-service/leetcode-api/aws-lambda-service/lambda.ts
@@ -1,3 +1,12 @@
-import serverlessExpress from "@vendia/serverless-express";
+import type { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import awsServerlessExpress from "aws-serverless-express";
 import app from "./app";
-export const handler = serverlessExpress({ app });
+
+const server = awsServerlessExpress.createServer(app);
+
+export const handler = (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
+};
